c4349a8587fb21385094fea3a82ccfa62923c7c8
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2022-12-01 10:33:14 +0100

    init

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+let thingsILike = [
+    "World of Warcraft",
+    "League of Legends",
+    "Cooking",
+    "Reading",
+    "TV Series",
+    "Animes",
+    "Metal Music",
+    "Coding",
+    "PHP",
+    "Beer",
+];
+
+const myWowCollection = [
+    {
+        'Name': "Gollumélock",
+        'Class': "Warlock",
+        'Spec': "Demonology",
+        'Race': "Dark Iron Dwarf",
+        'Faction': "Alliance", // Horde or Alliance
+        'Type': "DPS", // DPS, Healer or Tank
+        'First Time Played': "9.2",
+    },
+
+    {
+        'Name': "Gollumémonk",
+        'Class': "Monk",
+        'Spec': "Brewmaster",
+        'Race': "Dwarf",
+        'Faction': "Alliance", // Horde or Alliance
+        'Type': "Tank", // DPS, Healer or Tank
+        'First Time Played': "9.1.5",
+    },
+
+    {
+        'Name': "Gollumévok",
+        'Class': "Evoker",
+        'Spec': "Devastation",
+        'Race': "Dracthyr",
+        'Faction': "Alliance", // Horde or Alliance
+        'Type': "DPS", // DPS, Healer or Tank
+        'First Time Played': "10.0",
+    },
+
+    {
+        'Name': "Meguria",
+        'Class': "Priest",
+        'Spec': "Discipline",
+        'Race': "Draenei",
+        'Faction': "Alliance", // Horde or Alliance
+        'Type': "Healer", // DPS, Healer or Tank
+        'First Time Played': "9.0",
+    },
+
+    {
+        'Name': "Golluméo",
+        'Class': "Shaman",
+        'Spec': "Enhancement",
+        'Race': "Troll",
+        'Faction': "Horde", // Horde or Alliance
+        'Type': "DPS", // DPS, Healer or Tank
+        'First Time Played': "8.3",
+    },
+
+
+];
